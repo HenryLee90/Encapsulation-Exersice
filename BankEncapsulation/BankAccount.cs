c6bb2c0c530c72035f37dcacf82ac1e07f0c9ebc
Henry Lee <hlee09151@gmail.com>
2022-05-03 10:56:50 -0500

    init

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Create a new Console Application in your repos folder---done
+//name it BankEncapsulation---done
+//Create a new public class named BankAccount---done
+//In your BankAccount class, create the following:
+//A private field of type double named balance with a value of 0----done
+//Define a method named Deposit that will accept a double and store that value in the balance field-----done
+//Define a method named GetBalance that will return the amount stored in the balance field---done
+//In the main method of your application, create a new instance of the BankAccount class. Allow the user of the application to Deposit money and retrieve their balance through the console.---Done
+//Once finished save, commit, and push back to Github
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+        public void  Deposit(double money)
+        {
+            balance += money;
+            
+        }
+        public double GetBalance()
+        {
+            return balance;  
+
+            
+        }
+
+    }   
+}   
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            BankAccount account = new BankAccount();
+            // account Deposit += 5.00m;
+            account.Deposit(5.00);
+           var newBalance= account.GetBalance();
+            Console.WriteLine(newBalance);
+            account.Deposit(10.00);            
+            Console.WriteLine(account.GetBalance());
+            
+                
+            
+        }
+    }
+}
